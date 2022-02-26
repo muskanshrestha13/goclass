5f7ca08e6417ba4a4e1f8c2cd491a0d7fd47dbc8
Muskan Shrestha <77856295+muskanshrestha13@users.noreply.github.com>
2022-02-25 23:01:06 -0500

    01/21/2022 final

--- main.go
+++ main.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Printf("Hello !")
+var a int8 = -1
 
+const PI = 3.14
+
+func main() {
+	var testingUint8 uint8 = 1
+	c := "a"
+	a += 2      // a= a+2
+	b := 10 % 2 //remainder
+	x := 2 << 2 //0010(binary of 2)
+	testingUint8 = 2
+	fmt.Println(a)
+	fmt.Println(" VAL OF b =", b)
+	fmt.Println(" VAL OF x =", x)
+	fmt.Println(testingUint8)
+	fmt.Printf("%T\n", c)
+	add()
+
+}
+func add() {
+	c := 5
+	fmt.Println(c)
 }
